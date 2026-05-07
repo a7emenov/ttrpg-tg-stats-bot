47383ed48efcc056952b5341209d96f31b15670d
Anton Semenov <semenov.antony97@gmail.com>
2026-05-07 15:03:53 +0900

    Add dialogue state handler (#20)

---
+++ service/src/main/scala/com/github/a7emenov/api/telegram/dialoguestate/BotDialogueStateHandler.scala
@@ -0,0 +1,35 @@
+package com.github.a7emenov.api.telegram.dialoguestate
+
+import cats.MonadThrow
+import cats.syntax.applicative.*
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
+import com.bot4s.telegram.api.declarative.Action
+import com.github.a7emenov.domain.dialoguestate.DialogueState
+import com.github.a7emenov.domain.user.User
+import com.github.a7emenov.process.dialoguestate.DialogueStateProcess
+
+trait BotDialogueStateHandler[F[_]]:
+
+  implicit val monad: MonadThrow[F]
+
+  protected def dialogueStateProcess: DialogueStateProcess[F]
+
+  // todo: ensure this locks the state during processing.
+  /** Proceeds only on state matched by the partial function. Non-matching states are ignored.
+   *  New state returned from `action` is automatically updated.
+   */
+  def onDialogueState(userId: User.Id)(action: PartialFunction[
+    DialogueState,
+    F[DialogueState]
+  ])(onError: Action[F, DialogueStateProcess.Error.Get] = _ => ().pure[F]): F[Unit] =
+    dialogueStateProcess.get(userId).map {
+      case Right(state) =>
+        if action.isDefinedAt(state) then
+          action(state) >> dialogueStateProcess.update(userId, state)
+        else
+          ().pure[F]
+
+      case Left(error) =>
+        onError(error)
+    }
---
+++ service/src/main/scala/com/github/a7emenov/process/dialoguestate/DialogueStateProcessDefault.scala
@@ -0,0 +1,44 @@
+package com.github.a7emenov.process.dialoguestate
+
+import cats.Functor
+import cats.effect.{Ref, Resource, Sync}
+import cats.syntax.either.*
+import cats.syntax.functor.*
+import com.github.a7emenov.domain.dialoguestate.DialogueState
+import com.github.a7emenov.domain.gamesession.GameSession
+import com.github.a7emenov.domain.user.{User, UserRole}
+import com.github.a7emenov.process.dialoguestate
+import com.github.a7emenov.process.gamesession.GameSessionProcess
+import com.github.a7emenov.service
+import com.github.a7emenov.service.dialoguestate.DialogueStateService
+import com.github.a7emenov.service.gamesession.GameSessionService
+import DialogueStateProcessDefault.*
+
+class DialogueStateProcessDefault[F[_]: Functor](
+    dialogueStateService: DialogueStateService[F],
+) extends DialogueStateProcess[F]:
+
+  override def update(id: User.Id, state: DialogueState): F[Either[DialogueStateProcess.Error.Update, DialogueState]] =
+    dialogueStateService.update(id, state)
+      .map(_.leftMap(toUpdateError))
+
+  override def get(id: User.Id): F[Either[DialogueStateProcess.Error.Get, DialogueState]] =
+    dialogueStateService.get(id)
+      .map(_.leftMap(toGetError))
+
+object DialogueStateProcessDefault:
+
+  def make[F[_]: Functor](dialogueStateService: DialogueStateService[F]): Resource[F, DialogueStateProcess[F]] =
+    Resource.pure(DialogueStateProcessDefault(dialogueStateService))
+
+  private def toUpdateError(error: DialogueStateService.Error.Update): DialogueStateProcess.Error.Update =
+    error match {
+      case cause: DialogueStateService.Error.Update.System =>
+        DialogueStateProcess.Error.Update.System("Unknown dialogue state service record error", cause)
+    }
+
+  private def toGetError(error: DialogueStateService.Error.Get): DialogueStateProcess.Error.Get =
+    error match {
+      case cause: DialogueStateService.Error.Get.System =>
+        DialogueStateProcess.Error.Get.System("Unknown game session service get error", cause)
+    }
